69e71223d82e0c2852ec3d5729e15703d0d81c6e
Tomack <Tomack@TOMACK-KOMPUTER>
2016-08-28 12:14:26 +0200

    Add project files.

---
+++ src/InterferonBeta1bInjectionRecorder/appScripts/app.ts
@@ -0,0 +1,8 @@
+﻿import {Component} from '@angular/core';
+@Component({
+    selector: 'IFB1bInjectionRecorder',
+    templateUrl: 'appScripts/templates/app-template.html'
+})
+export class AppComponent {
+    Title = 'Betaferon injection recorder'
+}
